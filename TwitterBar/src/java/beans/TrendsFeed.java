23ccc91fc9452484baacc937fddc1b4e1410279b
utku <utku@172.17.3.15>
2016-11-21 23:39:25 -0600

    Trends Servlet added(not implemented yet)

--- TwitterBar/src/java/beans/TrendsFeed.java
+++ TwitterBar/src/java/beans/TrendsFeed.java
@@ -11,4 +11,25 @@ package beans;
  */
 public class TrendsFeed {
     
+    private String trendLink;
+    private String trendName;
+    
+    public TrendsFeed() {
+        
+    }
+    
+    public TrendsFeed(String tLink, String tName) {
+        trendLink = tLink;
+        trendName = tName;
+    }
+
+    public String getTrendLink() {
+        return trendLink;
+    }
+
+    public String getTrendName() {
+        return trendName;
+    }
+    
+    
 }
