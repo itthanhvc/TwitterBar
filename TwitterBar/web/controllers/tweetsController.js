26c364a22aeb987104dde80c7f2e1f1fdb97d983
ThanhVan <ThanhVan@10.10.53.80>
2016-11-24 00:13:31 -0600

    Merge origin/master
    Conflicts:
    	TwitterBar/web/views/tweets.html

--- TwitterBar/web/controllers/tweetsController.js
+++ TwitterBar/web/controllers/tweetsController.js
@@ -2,7 +2,9 @@
     // Controller when the main page/view loads
     tweetApp.controller("tweetsCtrl", ['$scope', 'DataService', function ($scope, DataService) {
             $scope.tweets = [];
+            $scope.topic = 'Documentary';
             $scope.handle = function (topic) {
+                $scope.topic = topic;
                 DataService.getTweets(topic).then(function (response) {
                     $scope.tweets = response;
                 });
