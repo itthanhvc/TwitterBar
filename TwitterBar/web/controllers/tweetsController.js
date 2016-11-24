b158d94e3da33d656f7ec83a31b3408edc39b297
ThanhVan <ThanhVan@10.10.53.80>
2016-11-24 00:16:57 -0600

    default value

--- TwitterBar/web/controllers/tweetsController.js
+++ TwitterBar/web/controllers/tweetsController.js
@@ -3,8 +3,14 @@
     tweetApp.controller("tweetsCtrl", ['$scope', 'DataService', function ($scope, DataService) {
             $scope.tweets = [];
             $scope.topic = 'Documentary';
+            getTweetFromServlet('Documentary');
+            /*Event handlers*/
             $scope.handle = function (topic) {
                 $scope.topic = topic;
+                getTweetFromServlet(topic);
+            };
+            /*Functions*/
+            function getTweetFromServlet(topic) {
                 DataService.getTweets(topic).then(function (response) {
                     $scope.tweets = response;
                 });
